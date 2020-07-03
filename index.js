7092a6faaa82d5e4b4e5576ace65b42e491dd389
Lakhan Khandelwal <lakhank143@gmail.com>
2020-07-03 19:35:10 +0530

    door lock

--- index.js
+++ index.js
@@ -1,88 +1,71 @@
 "use strict";
 
-const request = require('request');
+var Service, Characteristic
 
-var Service, Characteristic, HomebridgeAPI, url;
-
-module.exports = function(homebridge) {
-
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-inception", "InceptionSwitch", InceptionSwitch);
+module.exports = (homebridge) => {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-inception', 'InceptionSwitch', InceptionSwitch)
 }
 
-function InceptionSwitch(log, config) {
-  this.log = log;
-  this.name = config.name;
-  this.stateful = config.stateful;
-  this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
-  this._service = new Service.Switch(this.name);
-  this.UserID = '';
-  
-  this.cacheDirectory = HomebridgeAPI.user.persistPath();
-  this.storage = require('node-persist');
-  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
-  
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
-
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
-
-  if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
-	}
+class InceptionSwitch {
+  constructor (log, config) {
+    // get config values
+    this.log = log;
+    this.name = config['name'];
+    this.lockService = new Service.LockMechanism(this.name);
+    this.lockState = Characteristic.LockCurrentState.SECURED;
   }
 
-}
+  getServices () {
+    const informationService = new Service.AccessoryInformation()
+        .setCharacteristic(Characteristic.Manufacturer, 'Inner Range')
+        .setCharacteristic(Characteristic.Model, 'Inception Lock 1.0')
+        .setCharacteristic(Characteristic.SerialNumber, '1234');
 
-InceptionSwitch.prototype.getServices = function() {
-  return [this._service];
-}
+    this.lockService.getCharacteristic(Characteristic.LockCurrentState)
+      .on('get', this.getLockCharacteristicHandler.bind(this));
 
-InceptionSwitch.prototype._setOn = function(on, callback) {
+    this.lockService.getCharacteristic(Characteristic.LockTargetState)
+      .on('get', this.getLockCharacteristicHandler.bind(this))
+      .on('set', this.setLockCharacteristicHandler.bind(this));
 
-  this.log("Setting switch to " + on);
-
-    var options = {
-        'method': 'POST',
-        'url': 'http://121.200.28.54/api/v1/authentication/login',
-        'headers': {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"Username":"Neosoft","Password":"NeoSoft1!2"})
-    };
-
-    let temp;
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-        temp = response
-        // let temp = response.body.Response;
-
-        // if(temp.Result == 'Success' && temp.Message == 'OK') {
-        //     this.UserID = temp.UserID
-        // }
-    });
-    this.log("temp " + temp);
-  
-  if (on && !this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), this.time);
-  } else if (!on && this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), this.time);
+    return [informationService, this.lockService]
   }
-  
-  if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
+
+
+  actionCallback(err, result) {
+    if(err) {
+      this.updateCurrentState(Characteristic.LockCurrentState.JAMMED);
+      return console.error(err);
+    }
   }
-  
-  callback();
+
+  // Lock Handler
+  setLockCharacteristicHandler (targetState, callback) {
+    var lockh = this;
+
+    if (targetState == Characteristic.LockCurrentState.SECURED) {
+      this.log(`locking `+this.name, targetState)
+      this.lockState = targetState
+      this.updateCurrentState(this.lockState);
+      this.log(this.lockState+" "+this.name);
+    } else {
+      this.log(`unlocking `+this.name, targetState)
+      this.lockState = targetState
+      this.updateCurrentState(this.lockState);
+      this.log(this.lockState+" "+this.name);
+    }
+    callback();
+  }
+
+  updateCurrentState(toState) {
+    this.lockService
+      .getCharacteristic(Characteristic.LockCurrentState)
+      .updateValue(toState);
+  }
+
+  getLockCharacteristicHandler (callback) {
+    callback(null, this.lockState);
+  }
 }
