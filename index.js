e538ff72c11d84333e926ea024e4452c5f129043
Lakhan Khandelwal <lakhank143@gmail.com>
2020-07-03 11:24:35 +0530

    switch code added

--- index.js
+++ index.js
@@ -1,71 +1,63 @@
 "use strict";
 
-// var Service, Characteristic;
+var Service, Characteristic, HomebridgeAPI;
 
-// module.exports = function (homebridge) {
-//     Service = homebridge.hap.Service;
-//     Characteristic = homebridge.hap.Characteristic;
-//     homebridge.registerAccessory("homebridge-customer", "Custumer", CustomerAccessory);
-// };
+module.exports = function(homebridge) {
 
-// function CustomerAccessory(log, config) {
-//   this.log(this.config)
-// }
-
-"use strict";
-
-let Service, Characteristic, api;
-
-const packageJSON = require("./package.json");
-
-module.exports = function (homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-
-    api = homebridge;
-
-    homebridge.registerAccessory("homebridge-inception-panel", "INCEPTION-SENSOR", HTTP_INCEPTION);
-};
-
-function HTTP_INCEPTION(log, config) {
-    this.log = log;
-    // this.name = config.name;
-    // this.debug = config.debug || false;
-
-    config.getUrl = 'https//localhost:3000';
-    config.statusCache = 0
-    config.unit = 2;
-    config.statusPattern = '/(-?[0-9]{1,3}(\.[0-9])?)/';
-    config.notificationID = 1;
-    config.notificationPassword = '1234'
-
-    this.handleNotification.bind(config)
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  HomebridgeAPI = homebridge;
+  homebridge.registerAccessory("homebridge-inception-panel", "InceptionSwitch", InceptionSwitch);
 }
 
-HTTP_INCEPTION.prototype = {
+function InceptionSwitch(log, config) {
+  this.log = log;
+  this.name = config.name;
+  this.stateful = config.stateful;
+  this.reverse = config.reverse;
+  this.time = config.time ? config.time : 1000;		
+  this._service = new Service.Switch(this.name);
+  
+  this.cacheDirectory = HomebridgeAPI.user.persistPath();
+  this.storage = require('node-persist');
+  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
+  
+  this._service.getCharacteristic(Characteristic.On)
+    .on('set', this._setOn.bind(this));
 
-    identify: function (callback) {
-        this.log("Identify requested!");
-        callback();
-    },
+  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
 
-    getServices: function () {
-        if (!this.homebridgeService)
-            return [];
+  if (this.stateful) {
+	var cachedState = this.storage.getItemSync(this.name);
+	if((cachedState === undefined) || (cachedState === false)) {
+		this._service.setCharacteristic(Characteristic.On, false);
+	} else {
+		this._service.setCharacteristic(Characteristic.On, true);
+	}
+  }
+}
 
-        const informationService = new Service.AccessoryInformation();
+InceptionSwitch.prototype.getServices = function() {
+  return [this._service];
+}
 
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, "Inner Range")
-            .setCharacteristic(Characteristic.Model, "Inception Sensor")
-            .setCharacteristic(Characteristic.SerialNumber, "IS01")
-            .setCharacteristic(Characteristic.FirmwareRevision, packageJSON.version);
+InceptionSwitch.prototype._setOn = function(on, callback) {
 
-        return [informationService, this.homebridgeService];
-    },
-    
-    handleNotification: function(body) {
-        const characteristic = utils.getCharacteristic(this.homebridgeService, body.characteristic);
-        this.log("characteristic" + characteristic);
-    }
-};
+  this.log("Setting switch to " + on);
+
+  if (on && !this.reverse && !this.stateful) {
+    setTimeout(function() {
+      this._service.setCharacteristic(Characteristic.On, false);
+    }.bind(this), this.time);
+  } else if (!on && this.reverse && !this.stateful) {
+    setTimeout(function() {
+      this._service.setCharacteristic(Characteristic.On, true);
+    }.bind(this), this.time);
+  }
+  
+  if (this.stateful) {
+	this.storage.setItemSync(this.name, on);
+  }
+  
+  callback();
+}
